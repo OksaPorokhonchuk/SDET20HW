c674bb89e7434627509bb4969383c6d815d7e865
OksaPorokhonchuk <oksanap1305@gmail.com>
2024-08-13 22:12:00 -0400

    first commit

---
+++ src/src/main/java/org/example/e86/E86ClassAndObject.java
@@ -0,0 +1,19 @@
+package org.example.e86;
+
+public class E86ClassAndObject{
+    public static void main(String[] args) {
+
+
+
+            Student student= new Student();
+            student.name="John";
+            student.roll_no=2;
+        System.out.println("Name is " +student.name+ " and roll number is " +student.roll_no);
+
+
+
+
+
+
+    }
+}
